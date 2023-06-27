a7a98e06eefe69105c1d1a06b1ea14baf87481dc
kyaiooiayk <mura.gabriele.luigi@gmail.com>
2023-06-27 11:21:33 +0100

    added requirements_dev.txt and update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,30 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = "0.1.4"
+
 PACKAGE_NAME = "pyDecLog"
 AUTHOR = "kyaiooiayk"
 AUTHOR_EMAIL = "kayaiooiayk@email.com"
 URL = "https://github.com/kyaiooiayk/pyDecLog"
+DOWNLOAD_URL = "https://pypi.org/project/pyDecLog/"
 
 LICENSE = "MIT"
-DESCRIPTION = "pyDecLog: a python package for logging via decorators "
+VERSION = (HERE / "VERSION").read_text(encoding="utf8").strip()
+DESCRIPTION = "pyDecLog: a Python module for logging via decorators"
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
 
-INSTALL_REQUIRES = ["pympler", "numpy"]
+requirements = (HERE / "requirements.txt").read_text(encoding="utf8")
+INSTALL_REQUIRES = [s.strip() for s in requirements.split("\n")]
+
+dev_requirements = (HERE / "requirements_dev.txt").read_text(encoding="utf8")
+EXTRAS_REQUIRES = {"dev": [s.strip() for s in dev_requirements.split("\n")]}
+
+CLASSIFIERS = [
+    f"Programming Language :: Python :: 3.{str(v)}" for v in range(9, 10)
+]
+
+PYTHON_REQUIRES = ">=3.9"
 
 setup(
     name=PACKAGE_NAME,
@@ -26,7 +38,10 @@ setup(
     license=LICENSE,
     author_email=AUTHOR_EMAIL,
     url=URL,
+    python_requires=PYTHON_REQUIRES,
     install_requires=INSTALL_REQUIRES,
+    extra_requires=EXTRAS_REQUIRES,
     packages=find_packages(),
-    py_modules=["pyDecLog"]
+    py_modules=["pyDecLog"],
+    classifier=CLASSIFIERS,
 )
